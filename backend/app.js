b48d67dd9772cdac5b515e2ec2d2a618fa2b511d
“Elie <elie.maj@hotmail.com>
2022-07-16 18:27:29 +0300

    add login api

--- backend/app.js
+++ backend/app.js
@@ -19,3 +19,41 @@ module.exports = app;
 const User = require("./models/users");
 const Record = require("./models/records");
 const auth = require("./middleware/auth");
+
+// Login
+app.post("/login", async (req, res) => {
+    try {
+      // Get user input
+      const { email, password } = req.body;
+  
+      // Validate user input
+      if (!(email && password)) {
+        res.status(400).send("All input is required");
+      }
+      // Validate if user exist in our database
+      const user = await User.findOne({ email });
+  
+      if (user && (await bcrypt.compare(password, user.password))) {
+        // Create token
+        const token = jwt.sign(
+          { user_id: user._id, email },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
+  
+        // save user token
+        user.token = token;
+  
+        // user
+        res.status(200).json(user);
+      }
+      else {
+        res.status(400).send("Invalid Credentials");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  
