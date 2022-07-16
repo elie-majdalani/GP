9089b183928161d206e53cb51a27ffd76d549248
“Elie <elie.maj@hotmail.com>
2022-07-16 18:12:41 +0300

    add records model

---
+++ backend/models/records.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, default: null, unique: false},
+  name: { type: String, default: null },
+  category: { type: String, default: null },
+  discription: { type: String, default: null },
+  amount: { type: Number , default: null },
+  type :{ type: Boolean, default: null },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("record", userSchema);
