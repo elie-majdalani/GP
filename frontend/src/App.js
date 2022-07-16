118cf5d8a180fbc5428d20ec4701d670c6bfd29c
“Elie <elie.maj@hotmail.com>
2022-07-17 00:19:50 +0300

    add router with routes for signup and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Signup} from './pages/Signup';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { SignUp } from './pages/Signup';
+import { Login } from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/signup">Signup</Link>
+          <Link to="/login">Login</Link>
+        </header>
+        <Routes>
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
