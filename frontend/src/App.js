33208fe8aebf143b891eae972accee23c9480166
“Elie <elie.maj@hotmail.com>
2022-07-17 21:20:43 +0300

    frontend: add home linkings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { SignUp } from './pages/Signup';
 import { Login } from './pages/Login';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <Link to="/signup">Signup</Link>
-          <Link to="/login">Login</Link>
-        </header>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp/>} />
           <Route path="/login" element={<Login/>} />
         </Routes>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,8 @@
+export const Home = () => {
 return (
     <div>
         <a href="/signup">Signup</a>
         <a href="/login">Login</a>
     </div>
 )
+}
