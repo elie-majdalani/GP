fa78fa2c5cc1ab4ed9961b3d21436d0e11f679ce
“Elie <elie.maj@hotmail.com>
2022-07-18 13:29:16 +0300

    backend: add wallet to users schema

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,5 +1,16 @@
 const mongoose = require("mongoose");
 
+const walletSchema = new mongoose.Schema({
+  address: {type: String, default: null},
+  balance: {type: Number, default: 0},
+})
+
+const wallet = new mongoose.Schema({
+  eth:{type: walletSchema, default: {}},
+  trx:{type: walletSchema, default: {}},
+  usdt:{type: walletSchema, default: {}}
+});
+
 const userSchema = new mongoose.Schema({
   displayName: { type: String, default: null },
   email: { type: String, unique: true },
@@ -7,7 +18,7 @@ const userSchema = new mongoose.Schema({
   role: { type: String, default: "user" },
   photoURL: { type: String, default: null },
   token: { type: String },
-  walletAddress: { type: String, default: null },
+  wallet: { type: wallet , default: {} },
   createdAt: { type: Date, default: Date.now },
 });
 
