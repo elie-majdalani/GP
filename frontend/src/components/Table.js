0603c9e9af997c734f7421728466353830c1aae4
“Elie <elie.maj@hotmail.com>
2022-07-19 19:04:49 +0300

    frontend: add diplay records in the table function

---
+++ frontend/src/components/Table.js
@@ -0,0 +1,37 @@
+export const Table = ({totalRevenue,totalExpense,data}) => {
+    return(
+        <div>
+            <h1>Total Revenue: {totalRevenue}</h1>
+            <h1>Total Expense: {totalExpense}</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Discription</th>
+                        <th>Amount</th>
+                        <th>Type</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(item => {
+                        const date = item.createdAt
+                        const defaultDate = new Date(date.split('T'))
+                        return (
+                            <tr key={item._id}>
+                                <td>{item.name}</td>
+                                <td>{item.category}</td>
+                                <td>{item.discription}</td>
+                                <td>{item.amount}</td>
+                                <td>{item.type ? 'Revenue' : 'Expense'}</td>
+                                <td>{defaultDate.getDate() + "-" + parseInt(defaultDate.getMonth() + 1) + "-" + defaultDate.getFullYear()}</td>
+                            </tr>
+                        )
+                    }
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
