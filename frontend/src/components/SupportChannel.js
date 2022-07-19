a501b2c48d612209dd0445989b0de63bc51184a4
“Elie <elie.maj@hotmail.com>
2022-07-19 20:56:42 +0300

    frontend: add support chat retriever for each user

---
+++ frontend/src/components/SupportChannel.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import firebase from './firebase';
+import Messages from './Messages';
+
+const SupportChannel = ({ db, currentEmail, user }) => {
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+    const { email, displayName, photoURL } = user;
+    useEffect(() => {
+        if (db) {
+            //get messages for sepecific user
+            const messagesDB = db
+                .collection('messages')
+                .orderBy('createdAt')
+                .limit(100)
+                .onSnapshot(querySnapshot => {
+                    const data = querySnapshot.docs.map(doc => ({
+                        ...doc.data(),
+                        id: doc.id,
+                    })).filter(message => message.email === currentEmail || message.emailto === currentEmail)
+                    setMessages(data)
+                })
+            return messagesDB;
+        }
+    }, [db, currentEmail]);
+    const handleOnChange = (e) => {
+        setNewMessage(e.target.value);
+    }
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+
+        if (db) {
+            // add message to the database with the current user
+            db.collection('messages').add({
+                text: newMessage,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                displayName,
+                photoURL,
+                email,
+                emailto: currentEmail,
+            });
+            setNewMessage('');
+
+        }
+    }
+    return (
+        <div className="support-chat">
+            <ul>
+                {messages.map(message => (
+                    <li key={message.id}>
+                        <Messages {...message} />
+                    </li>
+                ))}
+            </ul>
+            <form onSubmit={handleOnSubmit}>
+                <input type="text" onChange={handleOnChange} value={newMessage} placeholder="Send Message" />
+                <button type="submit" disabled={!newMessage} onClick={handleOnSubmit}>Send</button>
+            </form>
+        </div>
+    )
+}
+export default SupportChannel;
