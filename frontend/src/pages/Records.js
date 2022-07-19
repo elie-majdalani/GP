73d76d395bc312fc80340331fb28371d334e202c
“Elie <elie.maj@hotmail.com>
2022-07-19 18:43:53 +0300

    frontend: add records variables

--- frontend/src/pages/Records.js
+++ frontend/src/pages/Records.js
@@ -1,18 +1,28 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { userContext } from '../components/userContext';
 import {Sidenav} from "../components/Sidenav";
 
 export const Records = () => {
     const { user } = useContext(userContext);
-    
+    const [charts, setCharts] = useState(false);
+    const [data, setData] = useState([]);
+    const [totalExpense, setTotalExpense] = useState(0);
+    const [totalRevenue, setTotalRevenue] = useState(0);
+    const [year, setYear] = useState(new Date().getFullYear());
+    const [chartYear, setChartYear] = useState(new Date().getFullYear());
+    const [oldest, setOldest] = useState(new Date());
+    const [month, setMonth] = useState(new Date().getMonth()+1);
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const [monthsRevenue,setMonthsRevenue] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    const [monthsExpense,setMonthsExpense] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
     return (
         <>
-            <Sidenav />
-            <div className="table">
-                <h1>Table</h1>
-                <p>User: {user.email}</p>
-            </div>
+        <Sidenav /> 
+        {charts} ?
+
+         : 
+         
         </>
     )
 }
