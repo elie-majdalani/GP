41694fe61547491ad1f2828707b578fb2a8936fe
“Elie <elie.maj@hotmail.com>
2022-07-19 16:24:43 +0300

    frontend: fix login with useContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,36 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { userContext } from './components/userContext';
 import { SignUp } from './pages/Signup';
 import { Login } from './pages/Login';
 import { Home } from './pages/Home';
 
 function App() {
+const [user, setUser] = useState(false);
+
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp/>} />
-          <Route path="/login" element={<Login/>} />
-        </Routes>
-      </div>
-    </Router>
+    <userContext.Provider value={{ user, setUser }}>
+      {user?
+        <Router>
+          <div>
+            {console.log(user)}
+            <button onClick={()=>{setUser(null)}}>Logout</button> 
+          </div>
+          <Routes> {/* <Route path="/" element={<Home />} /> */} </Routes>
+        </Router>
+      :<Router>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/signup" element={<SignUp/>} />
+              <Route path="/login" element={<Login/>} />
+            </Routes>
+          </div>
+        </Router>}
+
+    </userContext.Provider>
   );
 }
 
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,10 @@
 import { signInWithGoogle } from '../components/firebase';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import {login} from '../components/login';
+import { userContext } from '../components/userContext';
 
 export const Login = () => {
+    const {setUser} = useContext(userContext);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     return (
@@ -10,7 +12,7 @@ export const Login = () => {
             <h1>Login</h1>
             <input type="text" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} />
             <input type="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}} />
-            <button onClick={async()=>{await login(email,password)}}>Login</button>
+            <button onClick={async()=>{setUser(await login(email,password))}}>Login</button>
             <span>Don't have an account? <a href="/signup">Signup</a></span>
             <button className="button" onClick={signInWithGoogle}><i className="fab fa-google"></i>Sign in with google</button>
         </div>
