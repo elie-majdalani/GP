cbb7821185d509b413b32c84395ea3b7861d5076
“Elie <elie.maj@hotmail.com>
2022-07-20 23:54:50 +0300

    frontend: fetch crypto currency

--- frontend/src/components/UserContext.js
+++ frontend/src/components/UserContext.js
@@ -1,14 +1,18 @@
 import { createContext, useContext, useEffect, useState } from 'react'
 import { User } from '../components/fetchUser'
+import axios from 'axios'
 const AppContext = createContext()
 
 export const AppWrapper = ({
     children,
 }) => {
     const [user, setUser] = useState()
+    const [rate, setRate] = useState()
     useEffect(() => {
+        fetchRate()
         if (user) return
         fetchData()
+        
     }, [user])
     const fetchData = async () => {
         const token = localStorage.getItem('token')
@@ -19,9 +23,25 @@ export const AppWrapper = ({
             setUser()
         }
     }
+    const fetchRate = async () => {
+        const pricesEth = await axios.get(`https://api.cryptapi.io/eth/info/`);
+        const pricesTrx = await axios.get(`https://api.cryptapi.io/trx/info/`);
+        const pricesUsdt = await axios.get(`https://api.cryptapi.io/trc20/usdt/info/`);
+        const prices = {
+            ETH: pricesEth.data.prices.USD,
+            TRX: pricesTrx.data.prices.USD,
+            USDT: pricesUsdt.data.prices.USD
+        }
+        if (prices) {
+            setRate(prices)
+        } else {
+            setUser()
+        }
+    }
     return (
         <AppContext.Provider
             value={{
+                rate,
                 user,
                 setUser,
             }}>
