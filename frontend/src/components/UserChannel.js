8bcb22bddc9ef6a1f277e13392fcd437ae263f8d
“Elie <elie.maj@hotmail.com>
2022-07-20 23:54:25 +0300

    frontend: fix user channel

--- frontend/src/components/UserChannel.js
+++ frontend/src/components/UserChannel.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react';
 import firebase from './firebase';
 import Messages from './Messages';
+import { useAppContext } from './userContext';
 
-const UserChannel = ({ user = null, db = null }) => {
+const UserChannel = ({ db = null }) => {
+    const appData = useAppContext();
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState('');
-    const { email, displayName, photoURL } = user;
+
+
     useEffect(() => {
-        if (db) {
+        if (db && appData.user) {
             const unsubscribe = db
                 .collection('messages')
                 .orderBy('createdAt')
@@ -16,25 +19,26 @@ const UserChannel = ({ user = null, db = null }) => {
                     const data = querySnapshot.docs.map(doc => ({
                         ...doc.data(),
                         id: doc.id,
-                    })).filter(message => message.email === email || message.emailto === email)
+                    })).filter(message =>
+                        message.email === appData.user.email || message.emailto === appData.user.email
+                    )
                     setMessages(data);
                 })
-                    return unsubscribe;
-                }
+            return unsubscribe;
+        }
     }, [db]);
     const handleOnChange = (e) => {
         setNewMessage(e.target.value);
     }
     const handleOnSubmit = (e) => {
         e.preventDefault();
-
-        if (db) {
+        debugger
+        if (db && appData.user) {
             db.collection('messages').add({
                 text: newMessage,
                 createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-                displayName,
-                photoURL,
-                email
+                displayName: appData.user.displayName,
+                email: appData.user.email
             });
             setNewMessage('');
 
@@ -50,10 +54,8 @@ const UserChannel = ({ user = null, db = null }) => {
                     </li>
                 ))}
             </ul>
-            <form onSubmit={handleOnSubmit}>
-                <input type="text" onChange={handleOnChange} value={newMessage} placeholder="Send Message" />
-                <button type="submit" disabled={!newMessage} onClick={handleOnSubmit}>Send</button>
-            </form>
+            <input type="text" onChange={handleOnChange} value={newMessage} placeholder="Send Message" />
+            <button type="submit" disabled={!newMessage} onClick={handleOnSubmit}>Send</button>
         </div>
     )
 }
