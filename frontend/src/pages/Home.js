ed765948fe22cd5e6708bfac8a45bbd949d0d4a4
“Elie <elie.maj@hotmail.com>
2022-07-21 06:57:32 +0300

    syle add home finance monitoring slogan

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useAppContext } from '../components/userContext';
 import GP from '../assets/GP.png';
 import laptop from '../assets/laptop-v2.jpg';
+import notes from '../assets/Banknotes.png';
 // import stylesheet
 import '../styles/styles.css';
 export const Home = () => {
@@ -17,6 +18,9 @@ export const Home = () => {
             <div className="left-home-main-div">
                 <img id="home-gp" src={GP} alt='GP' />
                 <img id="home-laptop" src={laptop} alt='laptop' />
+                <img id="notes" src={notes} alt='notes' />
+                <span id="home-left-title">Finance Monitoring</span>
+                <span id="home-left-subtitle">Keep an eye on you income and expenditure using graphs and tables</span>
             </div>
             <div className="right-home-main-div">
                 <a href="/signup">Signup</a>
