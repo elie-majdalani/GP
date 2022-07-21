6a2d3b6d7f16949b0a7cc5715c6a4595f0a8e548
“Elie <elie.maj@hotmail.com>
2022-07-21 06:46:02 +0300

    syle add home laptop

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useAppContext } from '../components/userContext';
 import GP from '../assets/GP.png';
+import laptop from '../assets/laptop-v2.jpg';
 // import stylesheet
 import '../styles/styles.css';
 export const Home = () => {
@@ -14,10 +15,8 @@ export const Home = () => {
     return (
         <div className='hero-home-div'>
             <div className="left-home-main-div">
-                <div className="left-home-main-div-top">
-                    <img id="home-gp" src={GP} alt='GP' />
-                    
-                </div>
+                <img id="home-gp" src={GP} alt='GP' />
+                <img id="home-laptop" src={laptop} alt='laptop' />
             </div>
             <div className="right-home-main-div">
                 <a href="/signup">Signup</a>
