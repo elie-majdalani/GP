90918edb5315a5dd55652ef57bcd500b812d519d
“Elie <elie.maj@hotmail.com>
2022-07-21 06:24:26 +0300

    style home splitting the main left and right divs

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import { useAppContext } from '../components/userContext';
+// import stylesheet
+import '../styles/styles.css';
 export const Home = () => {
     const appdata = useAppContext();
     console.log(appdata);
@@ -9,15 +11,16 @@ export const Home = () => {
         }
     }, [appdata.user]);
     return (
-        <div>
+        <div className='hero-home-div'>
             <div className="left-home-main-div">
-
+                <div className="left-home-main-div-top">
+                    <img id="home-gp" src="../src/assets/GP.png" alt='' />
+                </div>
             </div>
             <div className="right-home-main-div">
                 <a href="/signup">Signup</a>
                 <a href="/login">Login</a>
             </div>
-
         </div>
     )
 }
