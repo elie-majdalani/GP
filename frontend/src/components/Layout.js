5ed98684c2e13f192fc66d3e509004f6b3ec7ff7
“Elie <elie.maj@hotmail.com>
2022-07-22 14:55:29 +0300

    frontend: add support chat link to the sidenav

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -2,25 +2,29 @@ import { useAppContext } from '../components/userContext';
 import { SideNav } from './Sidenav';
 import firebase from './firebase';
 import UserChannel from "../components/UserChannel";
+import { useState } from 'react';
+
 export const Layout = ({ children }) => {
+    const [isMessages, setIsMessages] = useState(true);
     const db = firebase.firestore();
     const appdata = useAppContext();
     return (
         appdata.user && appdata.user.role === "user" ? (
             <div>
                 <div className="layout">
-                    <SideNav />
+                    <SideNav setIsMessages={setIsMessages} isMessages={isMessages} />
                 </div>
                 <main>{children}</main>
                 <div className="user-info">
                     <span>{appdata.user.displayName}</span>
                     <button onClick={() => { appdata.setUser(); localStorage.clear(); window.location.href = '/login' }}>Logout</button>
-                    <UserChannel user={appdata.user} db={db} />
+                    {isMessages &&
+                    <UserChannel user={appdata.user} db={db} />}
                 </div>
             </div>
         ) : (
             <div>
-                {appdata.user&&(<button onClick={() => { appdata.setUser(); localStorage.clear(); window.location.href = '/login' }}>Logout</button>)}
+                {appdata.user && (<button onClick={() => { appdata.setUser(); localStorage.clear(); window.location.href = '/login' }}>Logout</button>)}
                 <main>{children}</main>
             </div>
         ))
