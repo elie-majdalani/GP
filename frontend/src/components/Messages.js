316a691241ab17491599a327f6750d6aac855cee
“Elie <elie.maj@hotmail.com>
2022-07-26 12:07:21 +0300

    frontend reefactor chat into seperate component

--- frontend/src/components/Messages.js
+++ frontend/src/components/Messages.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { formatRelative } from 'date-fns';
+import { useAppContext } from '../components/userContext';
 const Message = ({
     createdAt = null,
     text = '',
     displayName = '',
+    email = '',
     photoURL = '',
 }) => {
+    const appdata = useAppContext();
+    if(email === appdata.user.email){
     return (
-        <div>
+        <div className='user'>
             {displayName ? <p>{displayName}</p> : null}
             {createdAt?.seconds ? (
                 <span>
@@ -17,5 +21,19 @@ const Message = ({
             <p>{text}</p>
         </div>
     );
+            }
+    else{
+        return (
+            <div className='other'>
+                {displayName ? <p>{displayName}</p> : null}
+                {createdAt?.seconds ? (
+                    <span>
+                        {formatRelative(new Date(createdAt.seconds*1000), new Date())}
+                    </span>
+                ) : null}
+                <p>{text}</p>
+            </div>
+        );
+    }
 }
 export default Message;
