525021f91ed40f57ba069aeca753946e8da20997
“Elie <elie.maj@hotmail.com>
2022-07-26 12:42:08 +0300

    add logout btn logo

--- frontend/src/components/userInfo.js
+++ frontend/src/components/userInfo.js
@@ -5,7 +5,7 @@ export const UserInfo = () => {
         appdata.user &&
         <div className="user-info">
             <span id="user-name">{appdata.user.displayName}</span>
-            <button onClick={() => { appdata.setUser(); localStorage.clear(); window.location.href = '/login' }}>Logout</button>
+            <button id="logout-btn" onClick={() => { appdata.setUser(); localStorage.clear(); window.location.href = '/login' }}></button>
             <hr/>
         </div>
     )
