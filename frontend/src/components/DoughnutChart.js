859cb7a8534e84d3c7fd8bda1cb52c705dd080df
“Elie <elie.maj@hotmail.com>
2022-07-26 23:34:22 +0300

    frontend: fix doughnut chat sections colos

--- frontend/src/components/DoughnutChart.js
+++ frontend/src/components/DoughnutChart.js
@@ -12,9 +12,9 @@ const DoughnutChart = ({ totalExpense, totalRevenue }) => {
             label: 'My First Dataset',
             data: [totalExpense, totalRevenue, totalRevenue-totalExpense],
             backgroundColor: [
-              'rgb(255, 99, 132)',
-              'rgb(54, 162, 235)',
-              'rgb(255, 205, 86)'
+              'rgb(255, 0, 0)',
+              'rgb(0, 56, 255)',
+              'rgb(36, 255, 0)'
             ],
             hoverOffset: 4  
           }]
