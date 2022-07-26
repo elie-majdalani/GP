acc0e68774d796753790aa18d43c363d18d10858
“Elie <elie.maj@hotmail.com>
2022-07-26 16:34:50 +0300

    frontend: fix modal style sheet link

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 import { AddRecord } from "./AddRecord";
-import "../styles/modal.css";
+
 
 export const Modal = (props) => {
   return ReactDOM.createPortal(
